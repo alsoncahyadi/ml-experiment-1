9cdf03bba8eb3adf5d75f3e38aacf570d1baf1d9
ramosj.noah <ramosj.noah@gmail.com>
2017-10-30 13:31:20 +0700

    'init'

---
+++ read_dataset.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 13:14:50 2017
+
+@author: Fina
+"""
+
+import pandas as pd
+
+path = "DatasetEksperimen\CencusIncome\CencusIncome.data.txt"
+
+df = pd.read_csv(path)
+
